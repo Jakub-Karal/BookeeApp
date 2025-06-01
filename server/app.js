3662a64dda4566d56ce9854c4eeb4a066ed7aef5
Jakub Karal <karal.jakub@email.cz>
2025-06-01 22:39:43 +0200

    poslední detaily

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const clientController = require("./controller/clientController");
 const reservationController = require("./controller/reservationController");
+const listEndpoints = require("express-list-endpoints");
+const app = require("./app"); // nebo správná cesta k tvému app.js
+
+console.log(listEndpoints(app));
+
 
 const app = express();
 
